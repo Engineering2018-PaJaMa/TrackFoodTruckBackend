8ca6de5d63cf482a302d8daa08d578af1a35a34c
Maciej Gutenplan <202160@edu.p.lodz.pl>
2018-10-24 12:18:02 +0200

    Code reformat and dependency fix

--- rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/mongoDataBase/ConnectTODB.java
+++ rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/mongoDataBase/ConnectTODB.java
@@ -1,48 +1,54 @@
 package engineering.sonpm.trackfoodtruck.restWS.mongoDataBase;
 
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 
-public class ConnectTODB {
+public class ConnectTODB
+{
 
-    private static MongoClient mongo = new MongoClient( "localhost" , 27017 );
-    private static MongoCredential credential;
-    public static MongoDatabase database = mongo.getDatabase("TrackFoodTruck");
+	private static MongoClient mongo = new MongoClient("localhost", 27017);
+	private static MongoCredential credential;
+	public static MongoDatabase database = mongo.getDatabase("TrackFoodTruck");
 
-    public static void main( String args[] ) {
-        credentialsForDataBase();
-        listAllCollection();
-    }
+	public static void main(String args[])
+	{
+		credentialsForDataBase();
+		listAllCollection();
+	}
 
-    private static void credentialsForDataBase(){
+	private static void credentialsForDataBase()
+	{
 
-        // Creating Credentials
-        credential = MongoCredential.createCredential(
-                "FoodTrackMongo",
-                "TrackFoodTruck",
-                "AdminFoodTrack".toCharArray());
-        System.out.println("Connected to the database successfully");
-    }
+		// Creating Credentials
+		credential = MongoCredential.createCredential("FoodTrackMongo", "TrackFoodTruck", "AdminFoodTrack".toCharArray());
+		System.out.println("Connected to the database successfully");
+	}
 
-    public MongoCollection<Document> getCollection(String collectionName){
-        return database.getCollection(collectionName);
-    }
+	public MongoCollection<Document> getCollection(String collectionName)
+	{
+		return database.getCollection(collectionName);
+	}
 
-    public static void setDocument(MongoCollection<Document> collectionForDoc, Document docForSet){
-        collectionForDoc.insertOne(docForSet);
-    }
+	public static void setDocument(MongoCollection<Document> collectionForDoc, Document docForSet)
+	{
+		collectionForDoc.insertOne(docForSet);
+	}
 
-    public static void deleteDocument(MongoCollection<Document> collectionForDoc, Document docForDel){
-        collectionForDoc.deleteOne(docForDel);
-    }
+	public static void deleteDocument(MongoCollection<Document> collectionForDoc, Document docForDel)
+	{
+		collectionForDoc.deleteOne(docForDel);
+	}
 
-    private static void listAllCollection(){
-        for (String name : database.listCollectionNames()) {
-            System.out.println(name);
-        }
-    }
-    //TODO: Reszta funkcji oraz zabezpieczeń
+	private static void listAllCollection()
+	{
+		for (String name : database.listCollectionNames())
+		{
+			System.out.println(name);
+		}
+	}
+	//TODO: Reszta funkcji oraz zabezpieczeń
 }
