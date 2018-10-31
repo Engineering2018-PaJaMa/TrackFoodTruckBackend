37f0d6c09d2ed76ec43d905e219d86026f4bf493
gucio <gucio332@gmail.com>
2018-10-31 13:49:14 +0100

    Refactoring and style

--- rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/mongoDataBase/MongoDBController.java
+++ rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/mongoDataBase/MongoDBController.java
@@ -1,49 +1,40 @@
 package engineering.sonpm.trackfoodtruck.restWS.mongoDataBase;
 
-import com.mongodb.ServerAddress;
-import org.bson.Document;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class MongoDBController
-{
+public class MongoDBController {
 
-	private static MongoCredential credential = MongoCredential.createCredential("admin", "TrackFoodTruck", "Admin123".toCharArray());
-	private static MongoClient mongo = new MongoClient(new ServerAddress("localhost", 27017),Arrays.asList(credential));
-	public static MongoDatabase database = mongo.getDatabase("TrackFoodTruck");
+    private static MongoCredential credential = MongoCredential.createCredential("admin", "TrackFoodTruck", "Admin123".toCharArray());
+    private static MongoClient mongo = new MongoClient(new ServerAddress("localhost", 27017), Arrays.asList(credential));
+    public static MongoDatabase database = mongo.getDatabase("TrackFoodTruck");
 
-	public static void main(String args[])
-	{
-		listAllCollection();
-	}
+    public static void main(String args[]) {
+        listAllCollection();
+    }
 
-	public MongoCollection<Document> getCollection(String collectionName)
-	{
-		return database.getCollection(collectionName);
-	}
+    public MongoCollection<Document> getCollection(String collectionName) {
+        return database.getCollection(collectionName);
+    }
 
-	public static void setDocument(MongoCollection<Document> collectionForDoc, Document docForSet)
-	{
-		collectionForDoc.insertOne(docForSet);
-	}
+    public static void setDocument(MongoCollection<Document> collectionForDoc, Document docForSet) {
+        collectionForDoc.insertOne(docForSet);
+    }
 
-	public static void deleteDocument(MongoCollection<Document> collectionForDoc, Document docForDel)
-	{
-		collectionForDoc.deleteOne(docForDel);
-	}
+    public static void deleteDocument(MongoCollection<Document> collectionForDoc, Document docForDel) {
+        collectionForDoc.deleteOne(docForDel);
+    }
 
-	private static void listAllCollection()
-	{
-		for (String name : database.listCollectionNames())
-		{
-			System.out.println(name);
-		}
-	}
-	//TODO: Reszta funkcji oraz zabezpieczeń
+    private static void listAllCollection() {
+        for (String name : database.listCollectionNames()) {
+            System.out.println(name);
+        }
+    }
+    //TODO: Reszta funkcji oraz zabezpieczeń
 }
