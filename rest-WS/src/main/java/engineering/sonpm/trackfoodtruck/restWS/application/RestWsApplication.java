268a6bf37c7292eaf77e427147e15585cf6091e2
Gutek <gucio332@gmail.com>
2018-11-02 12:58:07 +0100

    Initialize function for when new database needed. // Credentail error - TODO

--- rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/application/RestWsApplication.java
+++ rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/application/RestWsApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import engineering.sonpm.trackfoodtruck.restWS.mongoDataBase.MongoDBController;
+
 @SpringBootApplication
 @ComponentScan("engineering.sonpm.trackfoodtruck.restWS")
 public class RestWsApplication
@@ -11,5 +13,6 @@ public class RestWsApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(RestWsApplication.class, args);
+		new MongoDBController().initializeDataBase();
 	}
 }
