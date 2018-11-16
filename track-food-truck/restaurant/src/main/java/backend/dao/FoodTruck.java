46d7712d21195b969a9045d0529921746c00da8c
Pawel Szopa <pawel.szopa@glase.se>
2018-11-16 15:55:31 +0100

    Merge branch 'master' into Lokalizacja-food-trucka
    # Conflicts:
    #	rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/controller/UserService.java
    #	rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/controller/UserServiceEndpoint.java
    #	rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/controller/UserServiceImpl.java
    #	track-food-truck/user/src/main/java/backend/controller/FoodTruckService.java
    #	track-food-truck/user/src/main/java/backend/controller/FoodTruckServiceEndpoint.java
    #	track-food-truck/user/src/main/java/backend/controller/UserService.java
    #	track-food-truck/user/src/main/java/backend/controller/UserServiceEndpoint.java
    #	track-food-truck/user/src/main/java/backend/controller/UserServiceImpl.java
    #	track-food-truck/user/src/main/java/backend/controller/user/UserService.java
    #	track-food-truck/user/src/main/java/backend/controller/user/UserServiceEndpoint.java
    #	track-food-truck/user/src/main/java/backend/controller/user/UserServiceImpl.java

--- track-food-truck/restaurant/src/main/java/backend/dao/FoodTruck.java
+++
@@ -1,8 +0,0 @@
-package backend.dao;
-
-/**
- * Created by Paweł Szopa on 31/10/2018
- */
-public class FoodTruck
-{
-}
---
+++ track-food-truck/user/src/main/java/user/backend/dto/User.java
@@ -0,0 +1,27 @@
+package user.backend.dto;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * Created by Paweł Szopa on 23/10/2018
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User
+{
+	@NonNull
+	private String id;
+	private String username;
+	private String password;
+	private String lastlogin;
+	private String name;
+	private String surname;
+	private int age;
+	private Set<String> favouriteFoodTrucks;
+}
