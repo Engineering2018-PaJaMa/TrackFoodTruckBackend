5773bdae6fbd27ee77f5e0ed664351e377a9de2a
Pawel Szopa <pawel.szopa@glase.se>
2018-11-16 15:36:52 +0100

    Merge branch 'master' into Lokalizacja-food-trucka
    # Conflicts:
    #	rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/controller/UserService.java
    #	rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/controller/UserServiceEndpoint.java
    #	rest-WS/src/main/java/engineering/sonpm/trackfoodtruck/restWS/controller/UserServiceImpl.java
    #	track-food-truck/user/src/main/java/backend/controller/FoodTruckService.java
    #	track-food-truck/user/src/main/java/backend/controller/FoodTruckServiceEndpoint.java
    #	track-food-truck/user/src/main/java/backend/controller/UserService.java
    #	track-food-truck/user/src/main/java/backend/controller/UserServiceEndpoint.java
    #	track-food-truck/user/src/main/java/backend/controller/UserServiceImpl.java
    #	track-food-truck/user/src/main/java/backend/controller/user/UserService.java
    #	track-food-truck/user/src/main/java/backend/controller/user/UserServiceEndpoint.java
    #	track-food-truck/user/src/main/java/backend/controller/user/UserServiceImpl.java

--- track-food-truck/user/src/main/java/backend/application/TrackFoodTruckApplication.java
+++ track-food-truck/user/src/main/java/backend/application/TrackFoodTruckApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class TrackFoodTruckApplication
 {
 	public static void main(String[] args)
