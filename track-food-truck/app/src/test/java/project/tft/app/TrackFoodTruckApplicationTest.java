d70f9f6fa91336280afa5b3f36f3dddf60d24763
Pawel Szopa <pawel.szopa@glase.se>
2018-11-20 11:48:50 +0100

    Removing redundant annotation.

--- track-food-truck/app/src/test/java/project/tft/app/TrackFoodTruckApplicationTest.java
+++ track-food-truck/app/src/test/java/project/tft/app/TrackFoodTruckApplicationTest.java
@@ -3,12 +3,10 @@ package project.tft.app;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TrackFoodTruckApplication.class)
-@ComponentScan(basePackages = {"project.tft.user.backend", "project.tft.restaurant.backend"})
 public class TrackFoodTruckApplicationTest
 {
 	@Test
